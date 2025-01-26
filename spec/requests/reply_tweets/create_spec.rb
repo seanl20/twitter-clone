66f68ff21619121b003b0b6dc43448819e1a29c2
sean <seanl20@yahoo.com>
2025-01-26 18:02:28 +1000

    feat: create reply tweet

---
+++ spec/requests/reply_tweets/create_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "POST /tweets/:tweet_id/reply_tweets", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+  let!(:tweet) { FactoryBot.create(:tweet, user:) }
+
+  context "when user is not signed in" do
+    it "redirect to sign in path" do
+      post tweet_reply_tweets_path(tweet)
+
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  context "when user is signed in" do
+    before { sign_in user }
+
+    it "succeed" do
+      post tweet_reply_tweets_path(tweet), params: {
+        tweet: {
+          body: "test"
+        }
+      }
+
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
+end
