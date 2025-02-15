12031ad0b966456433467121d1b1a127c97074f2
sean <seanl20@yahoo.com>
2025-02-16 01:56:34 +1000

    feat: add rendering of all message threads

--- app/controllers/message_threads_controller.rb
+++ app/controllers/message_threads_controller.rb
@@ -2,13 +2,6 @@ class MessageThreadsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    result = MessageThreads::Commands::CreateNew.new.call(user_id: params[:user_id])
-
-    case result
-    in Success(new_message_thread_user:)
-      @new_message_thread_user = new_message_thread_user
-    in Failure(:empty_params)
-      redirect_to root_path
-    end
+    @new_message_thread_user, @message_threads = MessageThreads::Queries::Get.new.call(user_id: params[:user_id], current_user:)
   end
 end
