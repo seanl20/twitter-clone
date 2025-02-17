82e7e3d785f61d3fadd8a047f7c0c2d885a48a87
Sean Lim <65902456+seanl20@users.noreply.github.com>
2025-02-17 23:45:03 +1000

    Revert "Revert "feat: change style for the messages""

--- app/lib/repositories/message_threads_user_repo.rb
+++ app/lib/repositories/message_threads_user_repo.rb
@@ -3,10 +3,5 @@ module Repositories
     def get_all_by_user(user:)
       MessageThreadsUser.where(user:)
     end
-
-    def get_by_user_and_message_thread(message_thread:, user:)
-      
-    endMessageThreadsUser.where(message_thread:, user:)
-    end
   end
 end
