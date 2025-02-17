0596afca4c48d45d8a5d57b7f05a00af9cd89972
Sean Lim <65902456+seanl20@users.noreply.github.com>
2025-02-17 23:38:53 +1000

    Revert "feat: change style for the messages"

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :message do
     association :user
-    association :message_thread
     body { "MyText" }
   end
 end
--- spec/intergration/repositories/message_repo_spec.rb
+++ spec/intergration/repositories/message_repo_spec.rb
@@ -33,38 +33,4 @@ RSpec.describe Repositories::MessageRepo do
       end
     end
   end
-
-  describe "#get_by_message_thread" do
-    subject(:get_by_message_thread) { described_class.new.get_by_message_thread(message_thread:) }
-
-    context "when mutliple message thread exists" do
-      context "when valid id are passed" do
-        let!(:message_thread) { FactoryBot.create(:message_thread) }
-        let!(:message_1) { FactoryBot.create(:message, message_thread:) }
-        let!(:message_2) { FactoryBot.create(:message, message_thread:) }
-        let!(:message_3) { FactoryBot.create(:message, message_thread:) }
-        let!(:message_4) { FactoryBot.create(:message, message_thread:) }
-
-        it "return message from message thread" do
-          expect(get_by_message_thread).to match_unordered_elements(message_1, message_2, message_3, message_4)
-        end
-      end
-
-      context "when invalid id are passed" do
-        let(:message_thread) { nil }
-
-        it "returns empty" do
-          expect(get_by_message_thread).to be_empty
-        end
-      end
-    end
-
-    context "message thread does not exists" do
-      let(:message_thread) { 1 }
-
-      it "returns empty" do
-        expect(get_by_message_thread).to be_empty
-      end
-    end
-  end
 end
