52b274b698b17996eabccad3e6d3ae8817969d7d
sean <seanl20@yahoo.com>
2025-02-25 00:24:07 +1000

    feat: rendering followed notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,4 +4,10 @@ class Notification < ApplicationRecord
   belongs_to :tweet, optional: true
 
   validates :verb, presence: true, inclusion: { in: Constants::Notification::VERBS_VALIDATION }
+
+  Constants::Notification::VERBS.each do |key, value|
+    define_method("#{key}?") {
+      value == verb
+    }
+  end
 end
